36d47a9f9449c13770d845fcbca652a66c696967
Abigail Amani Wanjiku <abigail.wanjiku@student.moringaschool.com>
2023-06-18 16:22:59 +0300

    test issues. Check and redo

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+var customerName = "bob"
+
+function upperCaseCustomerName() {
+     
+    return (customerName.toUpperCase());
+}
+function setBestCustomer(bestCustomer) {
+    var  bestCustomer= `not bob`;
+    return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+    setBestCustomer() = `something else`
+}
+
+const leastFavoriteCustomer = `Bob is being discriminated against`
+
+function changeLeastFavoriteCustomer(newName) {
+    let newName = "justice for Bob"
+    return leastFavoriteCustomer = newName
+}
